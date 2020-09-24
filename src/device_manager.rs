ecc08439b87a2fc4cf62ad818717f50a3ef4c881
francesco.cattoglio <francesco.cattoglio@gmail.com>
2020-09-24 11:38:53 +0200

    Changed presentation mode to immediate

--- src/device_manager.rs
+++ src/device_manager.rs
@@ -44,7 +44,7 @@ impl Manager {
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
             width: size.width,
             height: size.height,
-            present_mode: wgpu::PresentMode::Fifo,
+            present_mode: wgpu::PresentMode::Immediate,
         };
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
