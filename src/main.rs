32d8c7d022442b7b50bde41fae182b9f835c16f4
francesco.cattoglio <francesco.cattoglio@gmail.com>
2020-09-29 12:28:39 +0200

    First working example, crashes when resizing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
 };
+use imgui::*;
+use imgui_wgpu::Renderer;
+use imgui_winit_support;
 use serde::{Deserialize, Serialize};
 
 mod util;
@@ -56,6 +59,8 @@ fn main() {
         }
     };
 
+    wgpu_subscriber::initialize_default_subscriber(None);
+
     let event_loop = EventLoop::new();
     let mut builder = winit::window::WindowBuilder::new();
     builder = builder.with_title("test");
@@ -66,7 +71,38 @@ fn main() {
     }
     let window = builder.build(&event_loop).unwrap();
 
+    let mut hidpi_factor = window.scale_factor();
+
     let mut device_manager = device_manager::Manager::new(&window);
+
+    // Set up dear imgui
+    let mut imgui = imgui::Context::create();
+    let mut platform = imgui_winit_support::WinitPlatform::init(&mut imgui);
+    platform.attach_window(
+        imgui.io_mut(),
+        &window,
+        imgui_winit_support::HiDpiMode::Default,
+    );
+    imgui.set_ini_filename(None);
+
+    let font_size = (13.0 * hidpi_factor) as f32;
+    imgui.io_mut().font_global_scale = (1.0 / hidpi_factor) as f32;
+
+    imgui.fonts().add_font(&[FontSource::DefaultFontData {
+        config: Some(imgui::FontConfig {
+            oversample_h: 1,
+            pixel_snap_h: true,
+            size_pixels: font_size,
+            ..Default::default()
+        }),
+    }]);
+
+    let mut renderer = Renderer::new(&mut imgui, &device_manager.device, &mut device_manager.queue, rendering::SWAPCHAIN_FORMAT);
+    let mut last_frame = std::time::Instant::now();
+    let mut demo_open = true;
+
+    let mut last_cursor = None;
+
 
     let mut json_contents = String::new();
     let mut file = std::fs::File::open(&input_file).unwrap();
@@ -81,8 +117,8 @@ fn main() {
     //println!("debugged buffer contains {:?}", dbg_vect);
 
     // let renderer = renderer::Renderer::new(&device_manager, out_buffer_slice);
-    let mut renderer = rendering::SurfaceRenderer::new(&device_manager);
-    renderer.update_renderables(&device_manager, &chain);
+    let mut scene_renderer = rendering::SurfaceRenderer::new(&device_manager);
+    scene_renderer.update_renderables(&device_manager, &chain);
 
     let mut elapsed_time = std::time::Duration::from_secs(0);
     let mut old_instant = std::time::Instant::now();
@@ -131,14 +167,88 @@ fn main() {
             let mut frame = device_manager.swap_chain.get_current_frame()
                 .expect("could not get next frame");
 
-            renderer.render(&device_manager, &mut frame);
+            scene_renderer.render(&device_manager, &mut frame);
+
+            // imgui stuff
+            let delta_s = last_frame.elapsed();
+            last_frame = imgui.io_mut().update_delta_time(last_frame);
+
+            //let frame = match device_manager.swap_chain.get_current_frame() {
+            //    Ok(frame) => frame,
+            //    Err(e) => {
+            //        eprintln!("dropped frame: {:?}", e);
+            //        return;
+            //    }
+            //};
+            platform
+                .prepare_frame(imgui.io_mut(), &window)
+                .expect("Failed to prepare frame");
+            let ui = imgui.frame();
+
+            {
+                let window = imgui::Window::new(im_str!("Hello world"));
+                window
+                    .size([300.0, 100.0], Condition::FirstUseEver)
+                    .build(&ui, || {
+                        ui.text(im_str!("Hello world!"));
+                        ui.text(im_str!("This...is...imgui-rs on WGPU!"));
+                        ui.separator();
+                        let mouse_pos = ui.io().mouse_pos;
+                        ui.text(im_str!(
+                            "Mouse Position: ({:.1},{:.1})",
+                            mouse_pos[0],
+                            mouse_pos[1]
+                        ));
+                    });
+
+                let window = imgui::Window::new(im_str!("Hello too"));
+                window
+                    .size([400.0, 200.0], Condition::FirstUseEver)
+                    .position([400.0, 200.0], Condition::FirstUseEver)
+                    .build(&ui, || {
+                        ui.text(im_str!("Frametime: {:?}", delta_s));
+                    });
+
+                ui.show_demo_window(&mut demo_open);
+            }
+
+            let mut encoder: wgpu::CommandEncoder =
+                device_manager.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+            if last_cursor != Some(ui.mouse_cursor()) {
+                last_cursor = Some(ui.mouse_cursor());
+                platform.prepare_render(&ui, &window);
+            }
+
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.output.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Load,
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+
+            renderer
+                .render(ui.render(), &device_manager.queue, &device_manager.device, &mut rpass)
+                .expect("Rendering failed");
+
+            drop(rpass);
+
+            device_manager.queue.submit(Some(encoder.finish()));
         }
         Event::MainEventsCleared => {
             // RedrawRequested will only trigger once, unless we manually
             // request it.
             window.request_redraw();
         }
+        Event::RedrawEventsCleared => {
+        },
         _ => {}
-    }
+        }
+        platform.handle_event(imgui.io_mut(), &window, &event);
     });
 }
