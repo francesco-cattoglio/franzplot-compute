83a69dc88739d26b71162080f8b0946aabec6a54
francesco.cattoglio <francesco.cattoglio@gmail.com>
2021-01-13 17:40:29 +0100

    Added Bézier of degree 2 and 3

--- src/computable_scene/compute_block/bezier.rs
+++ src/computable_scene/compute_block/bezier.rs
@@ -36,13 +36,19 @@ impl BezierData {
         }
     }
 
-    fn get_point_data(processed_blocks: &ProcessedMap, id: BlockId) -> Result<&PointData, BlockCreationError> {
+    fn get_point_buffer(processed_blocks: &ProcessedMap, id: BlockId) -> Result<&wgpu::Buffer, BlockCreationError> {
         let found_element = processed_blocks.get(&id).ok_or(BlockCreationError::InternalError("Point input does not exist in the block map"))?;
         let block = found_element.as_ref().or(Err(BlockCreationError::InputNotBuilt(" Node not computed \n due to previous errors ")))?;
-        if let ComputeBlock::Point(data) = block {
-            Ok(data)
-        } else {
-            Err(BlockCreationError::InputInvalid(" the input provided to Bezier \n is not a Point "))
+        match block {
+            ComputeBlock::Point(data) => Ok(&data.out_buffer),
+            ComputeBlock::Transform(data) => {
+                if let Dimensions::D0 = data.out_dim {
+                    Ok(&data.out_buffer)
+                } else {
+                    Err(BlockCreationError::InputInvalid(" the input provided to Bezier \n is not a Point "))
+                }
+            },
+            _ => Err(BlockCreationError::InputInvalid(" the input provided to Bezier \n is not a Point ")),
         }
     }
 
@@ -55,8 +61,8 @@ impl BezierData {
         }
     }
     pub fn new_degree_1(device: &wgpu::Device, processed_blocks: &ProcessedMap, descriptor: BezierBlockDescriptor) -> Result<Self, BlockCreationError> {
-        let p0_data = Self::get_point_data(processed_blocks, descriptor.points[0])?;
-        let p1_data = Self::get_point_data(processed_blocks, descriptor.points[1])?;
+        let p0_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[0])?;
+        let p1_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[1])?;
 
         let param = Self::create_parameter(descriptor.quality);
 
@@ -81,7 +87,6 @@ void main() {{
     float t = index / ({n_points} - 1.0);
     vec4 f_t = (1-t) * p0 + t * p1;
     out_buff[index] = f_t;
-    out_buff[index].w = 1;
 }}
 "##, n_points=param.size);
 
@@ -92,11 +97,11 @@ void main() {{
             // add descriptor for input buffers
             CustomBindDescriptor {
                 position: 0,
-                buffer_slice: p0_data.out_buffer.slice(..)
+                buffer_slice: p0_buffer.slice(..)
             },
             CustomBindDescriptor {
                 position: 1,
-                buffer_slice: p1_data.out_buffer.slice(..)
+                buffer_slice: p1_buffer.slice(..)
             },
             // add descriptor for output buffer
             CustomBindDescriptor {
@@ -116,10 +121,144 @@ void main() {{
     }
 
     pub fn new_degree_2(device: &wgpu::Device, processed_blocks: &ProcessedMap, descriptor: BezierBlockDescriptor) -> Result<Self, BlockCreationError> {
-        unimplemented!();
+        let p0_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[0])?;
+        let p1_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[1])?;
+        let p2_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[2])?;
+
+        let param = Self::create_parameter(descriptor.quality);
+
+        let shader_source = format!(r##"
+#version 450
+layout(local_size_x = {n_points}, local_size_y = 1) in;
+
+layout(set = 0, binding = 0) buffer InputBuffer0 {{
+    vec4 p0;
+}};
+layout(set = 0, binding = 1) buffer InputBuffer1 {{
+    vec4 p1;
+}};
+layout(set = 0, binding = 2) buffer InputBuffer2 {{
+    vec4 p2;
+}};
+
+layout(set = 0, binding = 3) buffer OutputBuffer {{
+    vec4 out_buff[];
+}};
+
+void main() {{
+    uint index = gl_GlobalInvocationID.x;
+    float t = index / ({n_points} - 1.0);
+    vec4 f_t = (1-t)*(1-t)*p0 + (1-t)*t*p1 + t*t*p2;
+    out_buff[index] = f_t;
+}}
+"##, n_points=param.size);
+
+        let out_dim = Dimensions::D1(param);
+        let out_buffer = out_dim.create_storage_buffer(4 * std::mem::size_of::<f32>(), device);
+
+        let bindings = [
+            // add descriptor for input buffers
+            CustomBindDescriptor {
+                position: 0,
+                buffer_slice: p0_buffer.slice(..)
+            },
+            CustomBindDescriptor {
+                position: 1,
+                buffer_slice: p1_buffer.slice(..)
+            },
+            CustomBindDescriptor {
+                position: 2,
+                buffer_slice: p2_buffer.slice(..)
+            },
+            // add descriptor for output buffer
+            CustomBindDescriptor {
+                position: 3,
+                buffer_slice: out_buffer.slice(..)
+            }
+        ];
+
+        let (compute_pipeline, compute_bind_group) = compile_compute_shader(device, shader_source.as_str(), &bindings, None, Some("Bezier"))?;
+
+        Ok(Self {
+            compute_pipeline,
+            compute_bind_group,
+            out_buffer,
+            out_dim,
+        })
     }
     pub fn new_degree_3(device: &wgpu::Device, processed_blocks: &ProcessedMap, descriptor: BezierBlockDescriptor) -> Result<Self, BlockCreationError> {
-        unimplemented!();
+        let p0_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[0])?;
+        let p1_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[1])?;
+        let p2_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[2])?;
+        let p3_buffer = Self::get_point_buffer(processed_blocks, descriptor.points[3])?;
+
+        let param = Self::create_parameter(descriptor.quality);
+
+        let shader_source = format!(r##"
+#version 450
+layout(local_size_x = {n_points}, local_size_y = 1) in;
+
+layout(set = 0, binding = 0) buffer InputBuffer0 {{
+    vec4 p0;
+}};
+layout(set = 0, binding = 1) buffer InputBuffer1 {{
+    vec4 p1;
+}};
+layout(set = 0, binding = 2) buffer InputBuffer2 {{
+    vec4 p2;
+}};
+layout(set = 0, binding = 3) buffer InputBuffer3 {{
+    vec4 p3;
+}};
+
+layout(set = 0, binding = 4) buffer OutputBuffer {{
+    vec4 out_buff[];
+}};
+
+void main() {{
+    uint index = gl_GlobalInvocationID.x;
+    float t = index / ({n_points} - 1.0);
+    vec4 f_t = (1-t)*(1-t)*(1-t)*p0 + (1-t)*(1-t)*t*p1 + (1-t)*t*t*p2 + t*t*t*p3;
+    out_buff[index] = f_t;
+}}
+"##, n_points=param.size);
+
+        let out_dim = Dimensions::D1(param);
+        let out_buffer = out_dim.create_storage_buffer(4 * std::mem::size_of::<f32>(), device);
+
+        let bindings = [
+            // add descriptor for input buffers
+            CustomBindDescriptor {
+                position: 0,
+                buffer_slice: p0_buffer.slice(..)
+            },
+            CustomBindDescriptor {
+                position: 1,
+                buffer_slice: p1_buffer.slice(..)
+            },
+            CustomBindDescriptor {
+                position: 2,
+                buffer_slice: p2_buffer.slice(..)
+            },
+            CustomBindDescriptor {
+                position: 3,
+                buffer_slice: p3_buffer.slice(..)
+            },
+            // add descriptor for output buffer
+            CustomBindDescriptor {
+                position: 4,
+                buffer_slice: out_buffer.slice(..)
+            }
+        ];
+
+        let (compute_pipeline, compute_bind_group) = compile_compute_shader(device, shader_source.as_str(), &bindings, None, Some("Bezier"))?;
+
+        Ok(Self {
+            compute_pipeline,
+            compute_bind_group,
+            out_buffer,
+            out_dim,
+        })
     }
 
     pub fn encode(&self, encoder: &mut wgpu::CommandEncoder) {
