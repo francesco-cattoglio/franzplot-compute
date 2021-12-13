2f03648a316516868a6117f4daf4f9eb03a5fe8b
francesco.cattoglio <francesco.cattoglio@gmail.com>
2021-12-13 12:09:54 +0100

    Added same-parameter transformations

--- src/compute_graph/transform.rs
+++ src/compute_graph/transform.rs
@@ -28,20 +28,28 @@ pub fn create(
     match (&geometry_data, &matrix_data) {
         (Data::Geom0D{buffer: geom_buffer}, Data::Matrix0D{buffer: matrix_buffer})
             => t_0d_0d(device, geom_buffer, matrix_buffer),
+
         (Data::Geom0D{buffer: geom_buffer}, Data::Matrix1D{buffer, param})
             => t_0d_up_1d(device, geom_buffer, buffer, param),
+
         (Data::Geom1D{buffer, param}, Data::Matrix0D{buffer: matrix_buffer})
             => t_1d_1d(device, buffer, param, matrix_buffer),
+
         (Data::Geom1D{buffer, param}, Data::Matrix1D{buffer: matrix_buffer, param: matrix_param})
-            if !param.is_equal(matrix_param)? => t_1d_up_2d(device, buffer, param, matrix_buffer, matrix_param),
+            if !param.is_equal(matrix_param)?
+                => t_1d_up_2d(device, buffer, param, matrix_buffer, matrix_param),
+
         (Data::Geom1D{buffer, param}, Data::Matrix1D{buffer: matrix_buffer, param: matrix_param})
-            if param.is_equal(matrix_param)? => t_1d_same_param(device, buffer, matrix_buffer, matrix_param),
+            if param.is_equal(matrix_param)?
+                => t_1d_same_param(device, buffer, matrix_buffer, matrix_param),
+
         (Data::Geom2D{buffer, param1, param2}, Data::Matrix0D{buffer: matrix_buffer})
             => t_2d_2d(device, buffer, param1, param2, matrix_buffer),
+
         (Data::Geom2D{buffer, param1, param2}, Data::Matrix1D{buffer: matrix_buffer, param: matrix_param})
-            if true => t_2d_same_param(device, buffer, param1, param2, matrix_buffer),
-        (Data::Geom2D{buffer, param1, param2}, Data::Matrix1D{buffer: matrix_buffer, param: matrix_param})
-            if false => t_2d_same_param(device, buffer, param1, param2, matrix_buffer),
+            if param1.is_equal(matrix_param)? || param2.is_equal(matrix_param)?
+                => t_2d_same_param(device, buffer, param1, param2, matrix_buffer, matrix_param),
+
         (Data::Geom2D{buffer, param1, param2}, Data::Matrix1D{buffer: matrix_buffer, param: matrix_param})
             => Err(ProcessingError::IncorrectInput(" this operation would create \n an object with three parameters, \n which is not supported ")),
         _ => Err(ProcessingError::InternalError("unhandled transform case".into()))
@@ -227,17 +235,55 @@ fn t_1d_same_param(
     matrix_buffer: &wgpu::Buffer,
     param: &Parameter,
     ) -> SingleDataResult {
-    todo!()
-}
+    let wgsl_source = r##"
+[[block]] struct CurveBuffer {
+    positions: array<vec4<f32>>;
+};
 
-fn t_2d_same_param(
-    device: &wgpu::Device,
-    geom_buffer: &wgpu::Buffer,
-    geom_param1: &Parameter,
-    geom_param2: &Parameter,
-    matrix_buffer: &wgpu::Buffer,
-    ) -> SingleDataResult {
-    todo!()
+[[block]] struct MatrixBuffer {
+    matrices: array<mat4x4<f32>>;
+};
+
+[[group(0), binding(0)]] var<storage, read> in_curve: CurveBuffer;
+[[group(0), binding(1)]] var<storage, read> in_matrices: MatrixBuffer;
+[[group(0), binding(2)]] var<storage, read_write> output: CurveBuffer;
+
+[[stage(compute), workgroup_size(16)]]
+fn main([[builtin(global_invocation_id)]] global_id: vec3<u32>) {
+    let index = global_id.x;
+    output.positions[index] = in_matrices.matrices[index] * in_curve.positions[index];
+}
+"##.to_string();
+
+    // A point has a fixed size
+    let output_buffer = util::create_storage_buffer(device, std::mem::size_of::<glam::Vec4>() * param.n_points());
+    let bind_info = vec![
+        BindInfo {
+            buffer: geom_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: true },
+        },
+        BindInfo {
+            buffer: matrix_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: true },
+        },
+        BindInfo {
+            buffer: &output_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: false },
+        },
+    ];
+    let (pipeline, bind_group) = naga_compute_pipeline(device, &wgsl_source, &bind_info);
+
+    let operation = Operation {
+        bind_group,
+        pipeline: Rc::new(pipeline),
+        dim: [param.segments, 1, 1],
+    };
+    let new_data = Data::Geom1D {
+        buffer: output_buffer,
+        param: param.clone(),
+    };
+
+    Ok((new_data, operation))
 }
 
 fn t_1d_up_2d(
@@ -324,10 +370,6 @@ fn t_2d_2d(
     matrix: mat4x4<f32>;
 };
 
-[[block]] struct SurfaceBuffer {
-    positions: array<vec4<f32>>;
-};
-
 [[group(0), binding(0)]] var<storage, read> in_surface: SurfaceBuffer;
 [[group(0), binding(1)]] var<storage, read> in_matrix: MatrixBuffer;
 [[group(0), binding(2)]] var<storage, read_write> output: SurfaceBuffer;
@@ -345,7 +387,78 @@ fn main(
 "##.to_string();
 
     // A point has a fixed size
-    let output_buffer = util::create_storage_buffer(device, std::mem::size_of::<glam::Vec4>() * geom_param2.n_points() * geom_param1.n_points());
+    let output_buffer = util::create_storage_buffer(device, std::mem::size_of::<glam::Vec4>() * geom_param1.n_points() * geom_param2.n_points());
+    let bind_info = vec![
+        BindInfo {
+            buffer: geom_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: true },
+        },
+        BindInfo {
+            buffer: matrix_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: true },
+        },
+        BindInfo {
+            buffer: &output_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: false },
+        },
+    ];
+    let (pipeline, bind_group) = naga_compute_pipeline(device, &wgsl_source, &bind_info);
+
+    let operation = Operation {
+        bind_group,
+        pipeline: Rc::new(pipeline),
+        dim: [geom_param1.segments, geom_param2.segments, 1],
+    };
+    let new_data = Data::Geom2D {
+        buffer: output_buffer,
+        param1: geom_param1.clone(),
+        param2: geom_param2.clone(),
+    };
+
+    Ok((new_data, operation))
+}
+
+fn t_2d_same_param(
+    device: &wgpu::Device,
+    geom_buffer: &wgpu::Buffer,
+    geom_param1: &Parameter,
+    geom_param2: &Parameter,
+    matrix_buffer: &wgpu::Buffer,
+    matrix_param: &Parameter,
+    ) -> SingleDataResult {
+
+    let which_idx = if geom_param1.is_equal(matrix_param)? {
+        "par1_idx"
+    } else {
+        "par2_idx"
+    };
+    let wgsl_source = r##"
+[[block]] struct SurfaceBuffer {
+    positions: array<vec4<f32>>;
+};
+
+[[block]] struct MatrixBuffer {
+    matrices: array<mat4x4<f32>>;
+};
+
+[[group(0), binding(0)]] var<storage, read> in_surface: SurfaceBuffer;
+[[group(0), binding(1)]] var<storage, read> in_matrices: MatrixBuffer;
+[[group(0), binding(2)]] var<storage, read_write> output: SurfaceBuffer;
+
+[[stage(compute), workgroup_size(16, 16)]]
+fn main(
+    [[builtin(global_invocation_id)]] global_id: vec3<u32>,
+    [[builtin(num_workgroups)]] num_groups: vec3<u32>,
+) {
+    let par1_idx = global_id.x;
+    let par2_idx = global_id.y;
+    let index = par1_idx + num_groups.x * 16u * par2_idx;
+    output.positions[index] = in_matrices.matrices["##.to_string() + which_idx + r##"] * in_surface.positions[index];
+}
+"##;
+
+    // A point has a fixed size
+    let output_buffer = util::create_storage_buffer(device, std::mem::size_of::<glam::Vec4>() * geom_param1.n_points() * geom_param2.n_points());
     let bind_info = vec![
         BindInfo {
             buffer: geom_buffer,
