88030484b6031a40a9677d85f37075149c35295b
francesco.cattoglio <francesco.cattoglio@gmail.com>
2021-12-17 14:57:02 +0100

    Added transformations for primitives

--- src/compute_graph/transform.rs
+++ src/compute_graph/transform.rs
@@ -4,6 +4,8 @@ use std::rc::Rc;
 use super::Operation;
 use super::Parameter;
 use crate::computable_scene::globals::Globals;
+use crate::rendering::StandardVertexData;
+use crate::rendering::model::MODEL_CHUNK_VERTICES;
 use super::{SingleDataResult, ProcessingError};
 use super::{DataID, Data};
 use crate::util;
@@ -52,6 +54,13 @@ pub fn create(
 
         (Data::Geom2D{..}, Data::Matrix1D{..})
             => Err(ProcessingError::IncorrectInput(" this operation would create \n an object with three parameters, \n which is not supported ")),
+
+        (Data::Prefab { vertex_buffer, chunks_count, index_buffer, index_count }, Data::Matrix0D { buffer: matrix_buffer })
+            => t_prefab(device, vertex_buffer, *chunks_count, index_buffer, *index_count, matrix_buffer),
+
+        (Data::Prefab { .. }, Data::Matrix1D { .. })
+            => Err(ProcessingError::IncorrectInput(" parametric transforms on primitives \n are not allowed ")),
+
         _ => Err(ProcessingError::InternalError("unhandled transform case".into()))
     }
 
@@ -457,7 +466,6 @@ fn main(
 }
 "##;
 
-    // A point has a fixed size
     let output_buffer = util::create_storage_buffer(device, std::mem::size_of::<glam::Vec4>() * geom_param1.n_points() * geom_param2.n_points());
     let bind_info = vec![
         BindInfo {
@@ -488,3 +496,110 @@ fn main(
 
     Ok((new_data, operation))
 }
+
+
+fn t_prefab (device: &wgpu::Device,
+    vertex_buffer: &wgpu::Buffer,
+    chunks_count: usize,
+    index_buffer: &Rc<wgpu::Buffer>,
+    index_count: u32,
+    matrix_buffer: &wgpu::Buffer,
+    ) -> SingleDataResult {
+
+    let wgsl_source = format!(r##"
+struct MatcapVertex {{
+    position: vec4<f32>;
+    normal: vec4<f32>;
+    uv_coords: vec2<f32>;
+    padding: vec2<f32>;
+}};
+
+[[block]] struct PrefabBuffer {{
+    vertices: array<MatcapVertex>;
+}};
+
+[[block]] struct MatrixBuffer {{
+    matrix: mat4x4<f32>;
+}};
+
+[[group(0), binding(0)]] var<storage, read> in_prefab: PrefabBuffer;
+[[group(0), binding(1)]] var<storage, read> in_matrix: MatrixBuffer;
+[[group(0), binding(2)]] var<storage, read_write> output: PrefabBuffer;
+
+[[stage(compute), workgroup_size({vertices_per_chunk})]]
+fn main(
+    [[builtin(global_invocation_id)]] global_id: vec3<u32>,
+) {{
+    let index = global_id.x;
+
+    // in 3d->3d we operate on both the positions AND the normals.
+    // positions are simply multiplied by the matrix, while
+    // for normals, we extract the linear part of the transform
+    // and premultiply the normals by the inverse transpose,
+    // PROVIDED THE MATRIX IS NOT SINGULAR
+    // TODO: possible optimization: precompute inverse transpose
+    // directly in the matrix compute block (for 0D matrices only)
+    let A = mat3x3<f32>(
+        in_matrix.matrix[0].xyz,
+        in_matrix.matrix[1].xyz,
+        in_matrix.matrix[2].xyz,
+    );
+    let det: f32 = determinant(A);
+    if (det > 1e-6) {{
+        // WGSL does not provide any "inverse transpose" function, so compute it by hand
+        var inv_t: mat3x3<f32>;
+        let invdet = 1.0/det;
+        inv_t[0][0] =  (A[1][1] * A[2][2] - A[2][1] * A[1][2]) * invdet;
+        inv_t[1][0] = -(A[0][1] * A[2][2] - A[0][2] * A[2][1]) * invdet;
+        inv_t[2][0] =  (A[0][1] * A[1][2] - A[0][2] * A[1][1]) * invdet;
+        inv_t[0][1] = -(A[1][0] * A[2][2] - A[1][2] * A[2][0]) * invdet;
+        inv_t[1][1] =  (A[0][0] * A[2][2] - A[0][2] * A[2][0]) * invdet;
+        inv_t[2][1] = -(A[0][0] * A[1][2] - A[1][0] * A[0][2]) * invdet;
+        inv_t[0][2] =  (A[1][0] * A[2][1] - A[2][0] * A[1][1]) * invdet;
+        inv_t[1][2] = -(A[0][0] * A[2][1] - A[2][0] * A[0][1]) * invdet;
+        inv_t[2][2] =  (A[0][0] * A[1][1] - A[1][0] * A[0][1]) * invdet;
+
+        let transformed_normal: vec3<f32> = normalize(inv_t * in_prefab.vertices[index].normal.xyz);
+        output.vertices[index].normal = vec4<f32>(transformed_normal, 0.0);
+    }} else {{
+        // this is wrong, but at least it won't produce undefined garbage results
+        output.vertices[index].normal = in_prefab.vertices[index].normal;
+    }}
+    output.vertices[index].position = in_matrix.matrix * in_prefab.vertices[index].position;
+    output.vertices[index].uv_coords = in_prefab.vertices[index].uv_coords;
+    output.vertices[index].padding = in_prefab.vertices[index].padding;
+}}
+"##, vertices_per_chunk=MODEL_CHUNK_VERTICES,);
+
+    let output_buffer = util::create_storage_buffer(device, std::mem::size_of::<StandardVertexData>() * chunks_count * MODEL_CHUNK_VERTICES);
+    let bind_info = vec![
+        BindInfo {
+            buffer: vertex_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: true },
+        },
+        BindInfo {
+            buffer: matrix_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: true },
+        },
+        BindInfo {
+            buffer: &output_buffer,
+            ty: wgpu::BufferBindingType::Storage { read_only: false },
+        },
+    ];
+    let (pipeline, bind_group) = naga_compute_pipeline(device, &wgsl_source, &bind_info);
+
+    let operation = Operation {
+        bind_group,
+        pipeline: Rc::new(pipeline),
+        dim: [chunks_count as u32, 1, 1],
+    };
+    let new_data = Data::Prefab {
+        vertex_buffer: output_buffer,
+        chunks_count,
+        index_buffer: Rc::clone(index_buffer),
+        index_count,
+    };
+
+    Ok((new_data, operation))
+}
+
